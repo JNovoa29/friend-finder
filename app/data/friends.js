da095ac2fb72764ae49ad087e8edb7c69425092a
Jordan Novoa <jordan.novoa15@gmail.com>
2018-04-18 23:16:12 -0700

    added js

---
+++ app/data/friends.js
@@ -0,0 +1,22 @@
+var friendsArr = [
+    {
+        "name": "Ahmed",
+        "photo": "",
+        "scores": 
+        [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+
+]
+
+module.exports = friendsArr
---
+++ routing/apiRoutes.js
@@ -0,0 +1,50 @@
+var path = require('path')
+
+var friends = require('..data/friends.js')
+
+//export api routes
+module.exports = function(app) {
+    //list of friend entries
+    app.get('/api/friends', function(req, res) {
+        res.json(friends)
+    })
+    //add new friend
+    app.post('api/firends', function(req, res) {
+        var userInput = req.body
+
+        var userResponses = userInput.scores
+        //match
+        var userName = ''
+        var userImage = ''
+        var totalDifference = 10000
+
+        for ( var i = 0; i < friends.length; i++) {
+            var diff = 0
+            for (var j = 0; j < userResponses.length; j++) {
+                diff += Math.abs(friends[i].scores[j] - userResponses[j])
+            }
+            if (diff < totalDifference) {
+                totalDifference = diff
+                 = friends[i].name
+                userImage = friends[i].photo
+            }
+        }
+        friends.push(userInput)
+
+        res.json({status: 'OK', userName: userName, userImage: userImage})
+    })
+}
+
+
+
+
+
+
+
+app.get('/api/friends', function (req, res) {
+
+})
+
+app.post('/api/friends', function (req, res) {
+
+})
