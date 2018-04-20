2a3391d0fbff806f39ad3d6af3c796e8608e984e
Jordan Novoa <jordan.novoa15@gmail.com>
2018-04-19 23:55:26 -0700

    survey built, array built, matching in progress

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friendsArr = [
     {
-        "name": "Ahmed",
-        "photo": "",
+        "name": "Gal Gadot",
+        "photo": "https://ia.media-imdb.com/images/M/MV5BMjUzZTJmZDItODRjYS00ZGRhLTg2NWQtOGE0YjJhNWVlMjNjXkEyXkFqcGdeQXVyMTg4NDI0NDM@._V1_.jpg",
         "scores": 
         [
             5,
@@ -15,8 +15,193 @@ var friendsArr = [
             4,
             1
         ]
+    },
+    {
+        "name": "Michael Cera",
+        "photo": "https://ia.media-imdb.com/images/M/MV5BMTg0MjM4MDU1Nl5BMl5BanBnXkFtZTcwOTM3MTAzNw@@._V1_SY1000_CR0,0,721,1000_AL_.jpg",
+        "scores": 
+        [
+            2,
+            1,
+            2,
+            1,
+            5,
+            1,
+            2,
+            3,
+            3,
+            4
+        ]
+    },
+    {
+        "name": "Rachel McAdams",
+        "photo": "https://ia.media-imdb.com/images/M/MV5BMTY5ODcxMDU4NV5BMl5BanBnXkFtZTcwMjAzNjQyNQ@@._V1_UY317_CR2,0,214,317_AL_.jpg",
+        "scores": 
+        [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Marilyn Manson",
+        "photo": "https://ia.media-imdb.com/images/M/MV5BMjAyNDI3ODkxOF5BMl5BanBnXkFtZTYwMzk5NjY0._V1_UX214_CR0,0,214,317_AL_.jpg",
+        "scores": 
+        [
+            5,
+            4,
+            4,
+            4,
+            5,
+            5,
+            5,
+            5,
+            4,
+            3
+        ]
+    },
+    {
+        "name": "Gal Gadot",
+        "photo": "https://ia.media-imdb.com/images/M/MV5BMjUzZTJmZDItODRjYS00ZGRhLTg2NWQtOGE0YjJhNWVlMjNjXkEyXkFqcGdeQXVyMTg4NDI0NDM@._V1_.jpg",
+        "scores": 
+        [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Rob Schneider",
+        "photo": "https://ia.media-imdb.com/images/M/MV5BMTg0MDE4MTE0MF5BMl5BanBnXkFtZTcwOTYyOTM3MQ@@._V1_UX214_CR0,0,214,317_AL_.jpg",
+        "scores": 
+        [
+            1,
+            5,
+            2,
+            2,
+            3,
+            1,
+            4,
+            4,
+            4,
+            2
+        ]
+    },
+    {
+        "name": "Kit Harrington",
+        "photo": "https://ia.media-imdb.com/images/M/MV5BMTg3NTQ1NjQxNF5BMl5BanBnXkFtZTgwMDkwMzAyNDM@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
+        "scores": 
+        [
+            3,
+            2,
+            3,
+            5,
+            1,
+            4,
+            1,
+            2,
+            5,
+            5
+        ]
+    },
+    {
+        "name": "Olivia Wilde",
+        "photo": "https://ia.media-imdb.com/images/M/MV5BNjRmMThkZTItNGQ3Yy00NDc0LWE5OWMtYzkxMDRkMzI1OTcyXkEyXkFqcGdeQXVyMjQwMDg0Ng@@._V1_UX214_CR0,0,214,317_AL_.jpg",
+        "scores": 
+        [
+            3,
+            1,
+            3,
+            3,
+            3,
+            5,
+            1,
+            4,
+            4,
+            2
+        ]
+    },
+    {
+        "name": "Dwayne 'The Rock' Johnson",
+        "photo": "https://ia.media-imdb.com/images/M/MV5BMTkyNDQ3NzAxM15BMl5BanBnXkFtZTgwODIwMTQ0NTE@._V1_UX214_CR0,0,214,317_AL_.jpg",
+        "scores": 
+        [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            4,
+            4,
+            4,
+            4
+        ]
+    },
+    {
+        "name": "Chris Pratt",
+        "photo": "https://ia.media-imdb.com/images/M/MV5BNzg3MTgwOTgzMV5BMl5BanBnXkFtZTgwODIxMTUwMjE@._V1_UX214_CR0,0,214,317_AL_.jpg",
+        "scores": 
+        [
+            4,
+            3,
+            4,
+            2,
+            4,
+            3,
+            4,
+            2,
+            4,
+            3
+        ]
     }
 
 ]
+
+
+// //a more efficient method would be to create a loop for the questions
+// $(document).ready(function() {
+//     $('#submitButton').on('click', function(event) {
+//         event.preventDefault();
+        
+//         var userInput = {
+//             name: $('#userName').val().trim(),
+//             photo: $('#userImage').val().trim(),
+//             scores: [
+//                 $('#question1').val().trim(),
+//                 $('#question2').val().trim(),
+//                 $('#question3').val().trim(),
+//                 $('#question4').val().trim(),
+//                 $('#question5').val().trim(),
+//                 $('#question6').val().trim(),
+//                 $('#question7').val().trim(),
+//                 $('#question8').val().trim(),
+//                 $('#question9').val().trim(),
+//                 $('#question10').val().trim()
+//             ]
+//         }
+        
+//         $.post('/api/friends', userInput)
+//         .done(function(d) {
+//             $('#userMatch').html(data.matchName);
+//             $('#userMatchImage').attr('src', d.matchImage);
+//             $('#modal').modal('open')
+//         });
+//     });
+// })
 
 module.exports = friendsArr
